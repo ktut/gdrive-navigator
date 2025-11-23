ef0e8954a98894a7c7f6c01eed40310477c0d471
Ramzi Dreessen <ramzidreessen@gmail.com>
2025-11-23 12:39:51 -0600

    Use a wider range of numbers for ids

--- src/services/getMockData.ts
+++ src/services/getMockData.ts
@@ -5,10 +5,13 @@ export function getMockData(): File[] {
     const files: File[] = [];
     
     for (let i = 0; i < length; i++) {
-        const fileType: "file" | "folder" = Math.random() > 0.5 ? 'file' : 'folder';
+        const fileType: "file" | "folder" = Math.random() > 0.5 ? 'file' : 'folder',
+            minId = 1,
+            maxId = 10000;
+            // a real db would have a unique id for each file/folder, this will do for a demo
         files.push({
-            id: i + 1,
-            filename: fileType === 'file' ? `file-${i + 1}.txt` : `folder-${i + 1}`,
+            id: Math.floor(Math.random() * (maxId - minId + 1)) + minId,
+            filename: fileType === 'file' ? `file${i + 1}.txt` : `folder${i + 1}`,
             type: fileType,
             contents: null,
         });
