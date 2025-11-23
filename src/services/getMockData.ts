43d2bece91687f17045571aacde7c4c79cdd1c37
Ramzi Dreessen <ramzidreessen@gmail.com>
2025-11-23 12:51:59 -0600

    Update the mock data to be more reliable for simplicity

--- src/services/getMockData.ts
+++ src/services/getMockData.ts
@@ -1,18 +1,17 @@
 import { File } from '../types/files';
 
 export function getMockData(): File[] {
-    const length = Math.floor(Math.random() * 10) + 5; // Random length between 5-14
+    const length = 6; // Like the provided test data, this is a fixed length
     const files: File[] = [];
     
     for (let i = 0; i < length; i++) {
-        const fileType: "file" | "folder" = Math.random() > 0.5 ? 'file' : 'folder',
-            minId = 1,
+        const minId = 1,
             maxId = 10000;
             // a real db would have a unique id for each file/folder, this will do for a demo
         files.push({
             id: Math.floor(Math.random() * (maxId - minId + 1)) + minId,
-            filename: fileType === 'file' ? `file${i + 1}.txt` : `folder${i + 1}`,
-            type: fileType,
+            filename: i < 4 ? `file${i + 1}.txt` : `folder${i - 3}`, // 5 files, 6 folders
+            type: i < 4 ? 'file' : 'folder',
             contents: null,
         });
     }
